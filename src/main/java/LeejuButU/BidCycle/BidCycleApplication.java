d39a4c19d3b2f190873a0fd336e4b38ba49d32c0
ezeun <consistency1231@gmail.com>
2024-03-23 02:25:05 +0900

    init

---
+++ src/main/java/LeejuButU/BidCycle/BidCycleApplication.java
@@ -0,0 +1,13 @@
+package LeejuButU.BidCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LeejuButU/BidCycle/BidCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package LeejuButU.BidCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
