a8e0fd4553b7a10d4db300d5919db0898f957b90
kwon <kckc0608@naver.com>
2024-03-31 01:31:49 +0900

    fix: Member - ChatRoom 연관관계를 many-to-one 으로 변경
    * fix: Member - ChatRoom 연관관계를 many-to-one 으로 변경

--- src/main/java/LeejuButU/BidCycle/domain/chatroom/domain/ChatRoom.java
+++ src/main/java/LeejuButU/BidCycle/domain/chatroom/domain/ChatRoom.java
@@ -1,17 +1,16 @@
 package LeejuButU.BidCycle.domain.chatroom.domain;
 
 import jakarta.persistence.*;
-import org.springframework.data.util.Lazy;
 
 @Entity
 public class ChatRoom {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chatRoomId;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "seller_id", updatable = false, nullable = false)
     Member seller;
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "buyer_id", updatable = false, nullable = false)
     Member buyer;
 }
