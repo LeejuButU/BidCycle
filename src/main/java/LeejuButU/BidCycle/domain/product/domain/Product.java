17fde207df660de461c313c921cf7ca426978fe8
ezeun <consistency1231@gmail.com>
2024-04-01 00:51:19 +0900

    refactor : product 도메인 코드 수정 (#7)
    * nullable 설정

--- src/main/java/LeejuButU/BidCycle/domain/product/domain/Product.java
+++ src/main/java/LeejuButU/BidCycle/domain/product/domain/Product.java
@@ -11,19 +11,28 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String category;
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false)
     private LocalDateTime startDate;
+    @Column(nullable = false)
     private LocalDateTime endDate;
+    @Column(nullable = false)
     private Long startPrice;
+    @Column(nullable = false)
     private Long currentPrice;
+    @Column(nullable = false)
     private Long endPrice;
+    @Column(nullable = false)
     private String state;
     private LocalDateTime tradeDate;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "buyer_id", updatable = false, nullable = false)
+    @JoinColumn(name = "buyer_id", updatable = false)
     Member buyer;
 
     @OneToOne(fetch = FetchType.LAZY)
