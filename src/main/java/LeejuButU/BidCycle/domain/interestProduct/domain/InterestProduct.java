cb3801b6277bc8701e8c37c48b63624562d218b0
ezeun <consistency1231@gmail.com>
2024-04-02 23:19:50 +0900

    refactor: @Getter랑 private 추가

--- src/main/java/LeejuButU/BidCycle/domain/interestProduct/domain/InterestProduct.java
+++ src/main/java/LeejuButU/BidCycle/domain/interestProduct/domain/InterestProduct.java
@@ -3,8 +3,13 @@ package LeejuButU.BidCycle.domain.interestProduct.domain;
 import LeejuButU.BidCycle.domain.member.domain.Member;
 import LeejuButU.BidCycle.domain.product.domain.Product;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 public class InterestProduct {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,9 +17,9 @@ public class InterestProduct {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", updatable = false, nullable = false)
-    Member member;
+    private Member member;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", updatable = false, nullable = false)
-    Product product;
+    private Product product;
 }
