b6ecb918b89f9d35a5d24c3546201f88279aff87
kwon <kckc0608@naver.com>
2024-04-04 23:18:44 +0900

    feat: QnA Entity 작성
    * feat: Qna Entity 작성

---
+++ src/main/java/LeejuButU/BidCycle/domain/qna/domain/Qna.java
@@ -0,0 +1,37 @@
+package LeejuButU.BidCycle.domain.qna.domain;
+
+import LeejuButU.BidCycle.domain.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Qna {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long qnaId;
+
+    @NonNull
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @NonNull
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @NonNull
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @Column(length = 500)
+    private String answer;
+
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, updatable = false)
+    private Product product;
+}
