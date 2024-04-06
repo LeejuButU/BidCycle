f96a16c682bc7b6ebb1d7dd7aa6064ce3f9180ee
ezeun <consistency1231@gmail.com>
2024-04-07 00:04:52 +0900

    feat : MemberRepository 작성 (#20)

---
+++ src/main/java/LeejuButU/BidCycle/domain/member/repository/MemberRepository.java
@@ -0,0 +1,51 @@
+package LeejuButU.BidCycle.domain.member.repository;
+
+import LeejuButU.BidCycle.domain.member.domain.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    // CREATE
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    // READ
+    public Optional<Member> findByMemberId(Long memberId) {
+        Member member = em.find(Member.class, memberId);
+        return Optional.ofNullable(member);
+    }
+
+    public Optional<Member> findByNickname(String nickname) {
+        String jpql = "SELECT m FROM Member m WHERE m.nickname = :nickname";
+        Member member = em.createQuery(jpql, Member.class)
+                .setParameter("nickname", nickname)
+                .getSingleResult();
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll() {
+        String jpql = "SELECT m FROM Member m";
+        return em.createQuery(jpql, Member.class)
+                .getResultList();
+    }
+
+    // UPDATE
+    public void update(Member member){
+        em.merge(member);
+    }
+
+    // DELETE
+    public void delete(Member member){
+        em.remove(member);
+    }
+}
