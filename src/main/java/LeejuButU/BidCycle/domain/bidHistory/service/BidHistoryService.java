d0ea34869d53e0fc60ab37e8752664854afa5a03
kwon <kckc0608@naver.com>
2024-04-20 01:33:17 +0900

    feat: BidHistory Service 작성 (#48)
    * feat: BidHistory Service 작성
    
    * feat: BidHistoryRepository에 product 기준 bidHistory 전체 조회 로직 추가
    
    * feat: BidHistoryRepository에 product 기준 가장 높은 입찰가의 bidHistory 조회 로직 추가

---
+++ src/main/java/LeejuButU/BidCycle/domain/bidHistory/service/BidHistoryService.java
@@ -0,0 +1,60 @@
+package LeejuButU.BidCycle.domain.bidHistory.service;
+
+import LeejuButU.BidCycle.domain.bidHistory.domain.BidHistory;
+import LeejuButU.BidCycle.domain.bidHistory.repository.BidHistoryRepository;
+import LeejuButU.BidCycle.domain.member.domain.Member;
+import LeejuButU.BidCycle.domain.member.repository.MemberRepository;
+import LeejuButU.BidCycle.domain.product.domain.Product;
+import LeejuButU.BidCycle.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BidHistoryService {
+    private final BidHistoryRepository bidHistoryRepository;
+    private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+
+    // 입찰하기, 가장 최근 입찰보다 반드시 높은 가격을 불러야 함
+    @Transactional
+    public BidHistory bidProduct(Long bidderId, Long productId, long price) {
+        Member bidder = memberRepository.findById(bidderId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 멤버입니다.")
+        );
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 상품입니다.")
+        );
+
+        validateBidPriceIsGreaterThanBefore(product, price);
+
+        BidHistory bidHistory = BidHistory.builder()
+                .bidder(bidder)
+                .product(product)
+                .bidPrice(price)
+                .build();
+        bidHistoryRepository.save(bidHistory);
+        return bidHistory;
+    }
+
+    // 입찰 취소
+    public void deleteBidHistory(Long bidHistoryId) {
+        bidHistoryRepository.deleteById(bidHistoryId);
+    }
+
+    // 상품 하나에 대한 입찰 내역 데이터 조회
+    public List<BidHistory> findAllBidHistoryFromProduct(Product product) {
+        return bidHistoryRepository.findAllByProduct(product);
+    }
+
+    private void validateBidPriceIsGreaterThanBefore(Product product, long price) {
+        BidHistory bidHistory = bidHistoryRepository.findMostHighPriceBidHistoryInProduct(product);
+        if (price <= bidHistory.getBidPrice()) {
+            throw new IllegalArgumentException("가격은 반드시 이전 가격보다 높아야 합니다.");
+        }
+    }
+}
