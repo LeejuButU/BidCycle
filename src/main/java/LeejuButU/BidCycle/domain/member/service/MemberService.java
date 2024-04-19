49984316d41b79afd64781913f35d9cc656a4420
ezeun <consistency1231@gmail.com>
2024-04-20 01:23:06 +0900

    refactor : Member Service 수정 (#47)
    * refatcor : @Transactional(readOnly = false)는 괄호 부분 생략
    
    * refactor : findByIdOrThrow() 함수 작성해서 중복 제거

--- src/main/java/LeejuButU/BidCycle/domain/member/service/MemberService.java
+++ src/main/java/LeejuButU/BidCycle/domain/member/service/MemberService.java
@@ -16,8 +16,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    //회원ID로 회원 조회 (회원이 존재하지 않으면 예외 발생)
+    private Member findByIdOrThrow(Long memberId){
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+    }
+
     //회원 가입
-    @Transactional(readOnly = false)
+    @Transactional
     public Long join(Member member){
         if(memberRepository.existsById(member.getMemberId())){
             throw new IllegalStateException("이미 존재하는 회원입니다.");
@@ -33,15 +39,13 @@ public class MemberService {
 
     //회원 한 명 조회
     public Member findOne(Long memberId){
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+        return findByIdOrThrow(memberId);
     }
 
     // 회원 정보 변경
-    @Transactional(readOnly = false)
+    @Transactional
     public void updateMember(Long memberId, Member updatedMember) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+        Member member = findByIdOrThrow(memberId);
 
         // 변경할 정보를 새로운 정보로 업데이트
         member.updatePassword(updatedMember.getPassword());
@@ -53,10 +57,9 @@ public class MemberService {
     }
 
     // 회원 탈퇴
-    @Transactional(readOnly = false)
+    @Transactional
     public void withdrawMember(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+        Member member = findByIdOrThrow(memberId);
         memberRepository.delete(member);
     }
 
